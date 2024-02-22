652403ec434a3d0ce9199be596d767b6e718e9d3
KalanithiJP <kalanithi.jp@hyperverge.co>
2024-02-22 12:31:18 +0530

    Latest commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {NativeModules} from 'react-native';
+const {Hyperkyc} = NativeModules;
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button, StyleSheet, Text} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function startKYC(): void {
+  console.log('Launched');
+  let configDictionary: {[name: string]: any} = {};
+  configDictionary.appId = '63ac9a';
+  configDictionary.appKey = '<<appKey>>';
+  configDictionary.transactionId = 'HV-test-01';
+  configDictionary.workflowId = 'qr_scan';
+  //configDictionary.defaultLangCode = "vi";
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  let inputsDictionary: {[name: string]: string} = {};
+  inputsDictionary.transactionID =
+    '559687';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  //configDictionary.inputs = inputsDictionary
+
+  console.log(configDictionary);
+  Hyperkyc.launch(configDictionary, function (response: any) {
+    console.log(response);
+    switch (response.status) {
+      case 'auto_approved':
+      case 'auto_declined':
+      case 'needs_review':
+        // Handle Success Workflow
+        break;
+      case 'user_cancelled':
+        // Handle User Cancelled
+        break;
+      case 'error':
+        // Handle Failure Scenario
+        break;
+    }
+  });
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+function App(): JSX.Element {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <Button title="Start KYC" onPress={startKYC} />
+      
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
